d66ae1895dee0e049792e06f0c3501314f39de59
markbdifisow <hahahamark457@gmail.com>
2025-07-16 15:02:10 +0800

    Update nickname.js

--- script/event/nickname.js
+++ script/event/nickname.js
@@ -2,7 +2,7 @@ let config = {};
 try {
     config = require("../config.json");
 } catch (e) {
-    config.botName = "Keijo bot";
+    config.botName = "KEIJO - bot";
 }
 
 module.exports.config = {
