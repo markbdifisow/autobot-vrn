52d99ccfc682d0b711dd33f0e3433f7e483c9753
markbdifisow <hahahamark457@gmail.com>
2025-07-16 17:41:50 +0800

    Update vern.js

---
+++ script/vern.js
@@ -0,0 +1,18 @@
+module.exports.config = {
+  name: "hi",
+  version: "1.0.0",
+  credits: "vern",
+  description: "A simple command that sends a greeting message.",
+  commandCategory: "General",
+  usages: "[optional message]",
+  cooldowns: 5
+};
+
+module.exports.run = async function({ api, event, args }) {
+  try {
+    const msg = args.length ? args.join(" ") : "👋Hello po have a nice day i love you😘";
+    return api.sendMessage(msg, event.threadID);
+  } catch (err) {
+    console.error(err);
+  }
+};
